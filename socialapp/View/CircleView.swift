e802dab50f321b9da9018576ce383e3f1cf6ae5e
Kevin Sheng <Kevin@MacPro.local>
2017-08-21 16:47:38 -0400

    Feed UI Done

--- socialapp/View/CircleView.swift
+++ socialapp/View/CircleView.swift
@@ -1,21 +1,26 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  socialapp
 //
-//  Created by Kevin Sheng on 2017-08-04.
+//  Created by Kevin Sheng on 2017-08-21.
 //  Copyright © 2017 shengtech Inc. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
-    // Create a shadow border
+class CircleView: UIImageView {
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
+        
     }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = self.frame.width / 2
+    }
+
 }
